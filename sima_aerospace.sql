97f922bfb906e22d3f1950f5247d2d7e9fdca9b2
rizkyahf <rizky.agung.tif17@polban.ac.id>
2019-07-17 09:16:44 +0700

    update .sql file, please import using phpmyaadmin

--- sima_aerospace.sql
+++ sima_aerospace.sql
@@ -1,126 +1,196 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 17, 2019 at 04:15 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
 
- Source Server         : RizkyAHF - MySQL
- Source Server Type    : MySQL
- Source Server Version : 100110
- Source Host           : localhost:3306
- Source Schema         : sima_aerospace
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 100110
- File Encoding         : 65001
 
- Date: 02/07/2019 18:00:47
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Database: `sima_aerospace`
+--
 
--- ----------------------------
--- Table structure for admin
--- ----------------------------
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE `admin`  (
-  `username` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `password` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`username`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Records of admin
--- ----------------------------
-INSERT INTO `admin` VALUES ('admin', '21232f297a57a5a743894a0e4a801fc3');
+--
+-- Table structure for table `admin`
+--
 
--- ----------------------------
--- Table structure for inventarisasi_aset
--- ----------------------------
-DROP TABLE IF EXISTS `inventarisasi_aset`;
-CREATE TABLE `inventarisasi_aset`  (
-  `nomor_aset` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `nama` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `jenis` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+CREATE TABLE `admin` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventarisasi_aset`
+--
+
+CREATE TABLE `inventarisasi_aset` (
+  `nomor_aset` varchar(20) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `jenis` varchar(20) NOT NULL,
   `tanggal_pengadaan` date NOT NULL,
-  `deskripsi_kegunaan` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `tipe` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `keterangan` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `status` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `lokasi` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `penanggungjawab` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`nomor_aset`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  `deskripsi_kegunaan` text NOT NULL,
+  `tipe` varchar(20) NOT NULL,
+  `keterangan` text NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `lokasi` varchar(20) NOT NULL,
+  `penanggungjawab` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Records of inventarisasi_aset
--- ----------------------------
-INSERT INTO `inventarisasi_aset` VALUES ('3000753', 'V_BLOCK ', '3000 ', '2012-07-27', 'Block & Clamp UP V ', 'LPI-1688-1 ', 'ASDFG ', 'CIR ', 'Lemari 4-c ', 'IN STORE');
-INSERT INTO `inventarisasi_aset` VALUES ('300761', 'HEIGHT GAUGE ', '3000 ', '2012-07-27', 'Trimos Mini Vertical Height ', 'LPI-0621 ', 'QWERT ', 'VALID ', 'IN STORE ', 'OUT CELL SHAFT WA');
-INSERT INTO `inventarisasi_aset` VALUES ('300772', 'HOLTEST ', '3000 ', '2012-07-27', 'Tesa White Set 16 Pairs, 1 set ', 'LPI-1038 ', 'lkjh ', 'VALID ', 'R29-A ', 'OUT CELL COUPLING');
+--
+-- Dumping data for table `inventarisasi_aset`
+--
 
--- ----------------------------
--- Table structure for legal_audit
--- ----------------------------
-DROP TABLE IF EXISTS `legal_audit`;
-CREATE TABLE `legal_audit`  (
-  `nomor_aset` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `nomor_dokumen` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+INSERT INTO `inventarisasi_aset` (`nomor_aset`, `nama`, `jenis`, `tanggal_pengadaan`, `deskripsi_kegunaan`, `tipe`, `keterangan`, `status`, `lokasi`, `penanggungjawab`) VALUES
+('08009105', 'Test Input Aset', '3000', '2012-03-04', 'gunanya', 'Tipe Aset', 'Keterangan Barang 3', 'status ase', 'dirumah', 'PJ nya'),
+('3000753', 'V_BLOCK ', '3000 ', '2012-07-27', 'Block & Clamp UP V ', 'LPI-1688-1 ', 'ASDFG ', 'CIR ', 'Lemari 4-c ', 'IN STORE'),
+('3007729', 'HOLTEST update', '4000', '2012-07-26', 'Tesa White Set 16 Pairs, 1 set update', 'LPI-1039', 'update', 'VALID upda', 'R29-AU', 'OUT CELL COUPLING UPDATE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `legal_audit`
+--
+
+CREATE TABLE `legal_audit` (
+  `nomor_aset` varchar(20) NOT NULL,
+  `nomor_dokumen` varchar(20) NOT NULL,
   `tanggal_penetapan` date NOT NULL,
-  `foto_aset` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`nomor_dokumen`) USING BTREE,
-  INDEX `nomor_aset`(`nomor_aset`) USING BTREE,
-  CONSTRAINT `legal_audit_ibfk_1` FOREIGN KEY (`nomor_aset`) REFERENCES `inventarisasi_aset` (`nomor_aset`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  `foto_aset` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
--- ----------------------------
--- Records of legal_audit
--- ----------------------------
-INSERT INTO `legal_audit` VALUES ('300761', 'Kartu Garansi ', '2012-07-27', 'LPI-0621');
-INSERT INTO `legal_audit` VALUES ('300772', 'Kartu Kepemilikan ', '2012-07-27', 'LPI-1038');
-INSERT INTO `legal_audit` VALUES ('3000753', 'Sertikat ', '2012-07-27', 'LPI-1688-1');
+--
+-- Dumping data for table `legal_audit`
+--
 
--- ----------------------------
--- Table structure for optimasi_aset
--- ----------------------------
-DROP TABLE IF EXISTS `optimasi_aset`;
-CREATE TABLE `optimasi_aset`  (
-  `akusisi` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `kondisi` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `komponen_rusak` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `jenis_pemeliharaan` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `foto_optimasi` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `estimasi_biaya` int(11) NOT NULL,
-  INDEX `akusisi`(`akusisi`) USING BTREE,
-  CONSTRAINT `optimasi_aset_ibfk_1` FOREIGN KEY (`akusisi`) REFERENCES `penilaian_aset` (`akusisi`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+INSERT INTO `legal_audit` (`nomor_aset`, `nomor_dokumen`, `tanggal_penetapan`, `foto_aset`) VALUES
+('08009105', '1999', '2019-07-09', '5d253a9b92acb.jpg'),
+('3007729', 'Kartu Kepemilikan ', '2012-07-27', 'LPI-1038');
 
--- ----------------------------
--- Records of optimasi_aset
--- ----------------------------
-INSERT INTO `optimasi_aset` VALUES ('111940 ', 'Broken ', 'Body keropos ', 'Dempul bagian keropo', NULL, 1250000);
-INSERT INTO `optimasi_aset` VALUES ('230390 ', 'Broken ', 'Kain penutup ', 'Penggantian kain pen', NULL, 800000);
-INSERT INTO `optimasi_aset` VALUES ('480810 ', 'Broken ', 'Lamp ', 'Penggantian lampu', NULL, 1400000);
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for penilaian_aset
--- ----------------------------
-DROP TABLE IF EXISTS `penilaian_aset`;
-CREATE TABLE `penilaian_aset`  (
-  `nomor_aset` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `akusisi` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `akumulasi_depresiasi` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `nilai_akhir` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `BalShTm` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `ApCacct` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`akusisi`) USING BTREE,
-  INDEX `nomor_aset`(`nomor_aset`) USING BTREE,
-  CONSTRAINT `penilaian_aset_ibfk_1` FOREIGN KEY (`nomor_aset`) REFERENCES `inventarisasi_aset` (`nomor_aset`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+--
+-- Table structure for table `optimasi_aset`
+--
 
--- ----------------------------
--- Records of penilaian_aset
--- ----------------------------
-INSERT INTO `penilaian_aset` VALUES ('300761', '111940 ', '77903.29 ', '34036.71 ', '140 ', '153000');
-INSERT INTO `penilaian_aset` VALUES ('300772', '230390 ', '160337.13 ', '70052.87 ', '140 ', '153000');
-INSERT INTO `penilaian_aset` VALUES ('3000753', '480810 ', '334613.87 ', '146196.13 ', '140 ', '153000');
+CREATE TABLE `optimasi_aset` (
+  `akusisi` varchar(20) NOT NULL,
+  `kondisi` varchar(20) NOT NULL,
+  `komponen_rusak` varchar(20) NOT NULL,
+  `jenis_pemeliharaan` varchar(20) NOT NULL,
+  `foto_optimasi` varchar(20) DEFAULT NULL,
+  `estimasi_biaya` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Dumping data for table `optimasi_aset`
+--
+
+INSERT INTO `optimasi_aset` (`akusisi`, `kondisi`, `komponen_rusak`, `jenis_pemeliharaan`, `foto_optimasi`, `estimasi_biaya`) VALUES
+('4808109', 'Broken ', 'Lamp ', 'Penggantian lampu', NULL, 1400000),
+('AK-TEST', 'bagus', 'gaada', 'didiemin', '5d1c897ab7117.jpg', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penilaian_aset`
+--
+
+CREATE TABLE `penilaian_aset` (
+  `nomor_aset` varchar(20) NOT NULL,
+  `akusisi` varchar(100) NOT NULL,
+  `akumulasi_depresiasi` varchar(10) NOT NULL,
+  `nilai_akhir` varchar(20) NOT NULL,
+  `BalShTm` varchar(20) NOT NULL,
+  `ApCacct` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `penilaian_aset`
+--
+
+INSERT INTO `penilaian_aset` (`nomor_aset`, `akusisi`, `akumulasi_depresiasi`, `nilai_akhir`, `BalShTm`, `ApCacct`) VALUES
+('3000753', '4808109', '334613.879', '146196.139', '1409', '1530009'),
+('08009105', 'AK-TEST', '974818', '9999', '120', '120');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`) USING BTREE;
+
+--
+-- Indexes for table `inventarisasi_aset`
+--
+ALTER TABLE `inventarisasi_aset`
+  ADD PRIMARY KEY (`nomor_aset`) USING BTREE;
+
+--
+-- Indexes for table `legal_audit`
+--
+ALTER TABLE `legal_audit`
+  ADD PRIMARY KEY (`nomor_dokumen`) USING BTREE,
+  ADD KEY `nomor_aset` (`nomor_aset`) USING BTREE;
+
+--
+-- Indexes for table `optimasi_aset`
+--
+ALTER TABLE `optimasi_aset`
+  ADD KEY `akusisi` (`akusisi`) USING BTREE;
+
+--
+-- Indexes for table `penilaian_aset`
+--
+ALTER TABLE `penilaian_aset`
+  ADD PRIMARY KEY (`akusisi`) USING BTREE,
+  ADD KEY `nomor_aset` (`nomor_aset`) USING BTREE;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `legal_audit`
+--
+ALTER TABLE `legal_audit`
+  ADD CONSTRAINT `legal_audit_ibfk_1` FOREIGN KEY (`nomor_aset`) REFERENCES `inventarisasi_aset` (`nomor_aset`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `optimasi_aset`
+--
+ALTER TABLE `optimasi_aset`
+  ADD CONSTRAINT `optimasi_aset_ibfk_1` FOREIGN KEY (`akusisi`) REFERENCES `penilaian_aset` (`akusisi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `penilaian_aset`
+--
+ALTER TABLE `penilaian_aset`
+  ADD CONSTRAINT `penilaian_aset_ibfk_1` FOREIGN KEY (`nomor_aset`) REFERENCES `inventarisasi_aset` (`nomor_aset`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
